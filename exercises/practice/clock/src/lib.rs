fa4513170113f30ba1bd0599e1f74cb20236afbb
zacksleo <zacksleo@gmail.com>
2021-06-15 16:40:00 +0800

    feat(main): add clock

--- exercises/practice/clock/src/lib.rs
+++ exercises/practice/clock/src/lib.rs
@@ -1,15 +1,32 @@
-pub struct Clock;
+use std::fmt::Display;
+
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    hours: u8,
+    minutes: u8,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        let mut minutes = minutes + hours * 60;
+        while minutes < 0 {
+            minutes += 60 * 24
+        }
+        let min = minutes % 60;
+        let hours = minutes / 60 % 24;
+        Clock {
+            hours: hours as u8,
+            minutes: min as u8,
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        Clock::new(self.hours as i32, self.minutes as i32 + minutes)
+    }
+}
+
+impl Display for Clock {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
