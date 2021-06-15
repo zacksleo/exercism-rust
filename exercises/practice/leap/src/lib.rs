028c21d0fdbc7e7e693722603445bbb210e55e8a
zacksleo <zacksleo@gmail.com>
2021-06-15 23:04:28 +0800

    refactor(leap): update leap

--- exercises/practice/leap/src/lib.rs
+++ exercises/practice/leap/src/lib.rs
@@ -1,12 +1,3 @@
 pub fn is_leap_year(year: u64) -> bool {
-
-    if year % 400 == 0 {
-        true
-    } else if year % 4 != 0 {
-        false
-    } else if year % 100 == 0 {
-        false
-    } else {
-        true
-    }
+    year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)
 }
