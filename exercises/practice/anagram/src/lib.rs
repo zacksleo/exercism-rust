ec593398c9ad07eb448038fc7936c79c0b2a1f47
zacksleo <zacksleo@gmail.com>
2021-06-15 15:26:50 +0800

    feat(main): add practices

--- exercises/practice/anagram/src/lib.rs
+++ exercises/practice/anagram/src/lib.rs
@@ -1,9 +1,20 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+fn sort(word: &str) -> String {
+    let mut chars: Vec<char> = word.chars().collect();
+    chars.sort_unstable();
+    chars.into_iter().collect()
+}
+
+pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    let word = word.to_lowercase();
+    let sorted = sort(&word);
+    possible_anagrams
+        .iter()
+        .filter(|e| {
+            let x = e.to_lowercase();
+            x != word && sorted == sort(&x)
+        })
+        .cloned()
+        .collect()
 }
--- exercises/practice/gigasecond/src/lib.rs
+++ exercises/practice/gigasecond/src/lib.rs
@@ -1,6 +1,6 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, TimeZone, Utc};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    unimplemented!("What time is a gigasecond later than {}", start);
+    return Utc.timestamp_millis(start.timestamp_millis() + 1_000_000_000_000);
 }
