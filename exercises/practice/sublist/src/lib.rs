4b923e71fd222ea1f29a1c339cfff412d63030ab
zacksleo <zacksleo@gmail.com>
2021-06-19 21:27:28 +0800

    refact(sublist): add contains

--- exercises/practice/sublist/src/lib.rs
+++ exercises/practice/sublist/src/lib.rs
@@ -6,33 +6,23 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    if _first_list.is_empty() && _second_list.is_empty() {
+fn contains<T: PartialEq>(a: &[T], b: &[T]) -> bool {
+    if a.len() < b.len() {
+        return false;
+    }
+    if a.starts_with(b) {
+        return true;
+    }
+    contains(&a[1..], b)
+}
+
+pub fn sublist<T: PartialEq>(a: &[T], b: &[T]) -> Comparison {
+    if a == b {
         return Comparison::Equal;
-    } else if _first_list.is_empty() {
-        return Comparison::Sublist;
-    } else if _second_list.is_empty() {
+    } else if contains(a, b) {
         return Comparison::Superlist;
+    } else if contains(b, a) {
+        return Comparison::Sublist;
     }
-    if _first_list.len() == _second_list.len() {
-        match _first_list == _second_list {
-            true => return Comparison::Equal,
-            false => return Comparison::Unequal,
-        }
-    } else if _first_list.len() > _second_list.len() {
-        // [0,1,2,3,4,5,6]
-        // [1,2,3]
-        for i in 0..(_first_list.len()-_second_list.len()+1) {
-           if &_first_list[i..i+_second_list.len()] == _second_list {
-               return Comparison::Superlist;
-           }
-        }
-        return Comparison::Unequal;
-    }
-    for i in 0..(_second_list.len()-_first_list.len()+1) {
-        if &_second_list[i..i+_first_list.len()] == _first_list {
-            return Comparison::Sublist;
-        }
-     }
-     return Comparison::Unequal;
+    return Comparison::Unequal;
 }
