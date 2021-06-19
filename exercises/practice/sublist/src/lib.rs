695b198a5b0106d2c872c3246219bfa2c84fa994
zacksleo <zacksleo@gmail.com>
2021-06-19 21:10:47 +0800

    feat(src): add lib

--- exercises/practice/sublist/src/lib.rs
+++ exercises/practice/sublist/src/lib.rs
@@ -7,5 +7,32 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    if _first_list.is_empty() && _second_list.is_empty() {
+        return Comparison::Equal;
+    } else if _first_list.is_empty() {
+        return Comparison::Sublist;
+    } else if _second_list.is_empty() {
+        return Comparison::Superlist;
+    }
+    if _first_list.len() == _second_list.len() {
+        match _first_list == _second_list {
+            true => return Comparison::Equal,
+            false => return Comparison::Unequal,
+        }
+    } else if _first_list.len() > _second_list.len() {
+        // [0,1,2,3,4,5,6]
+        // [1,2,3]
+        for i in 0..(_first_list.len()-_second_list.len()+1) {
+           if &_first_list[i..i+_second_list.len()] == _second_list {
+               return Comparison::Superlist;
+           }
+        }
+        return Comparison::Unequal;
+    }
+    for i in 0..(_second_list.len()-_first_list.len()+1) {
+        if &_second_list[i..i+_first_list.len()] == _first_list {
+            return Comparison::Sublist;
+        }
+     }
+     return Comparison::Unequal;
 }
