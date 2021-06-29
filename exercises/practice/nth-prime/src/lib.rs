c78ee0448048830176b4d4d106312dd282cb21d6
zacksleo <zacksleo@gmail.com>
2021-06-29 23:13:47 +0800

    feat(practice): add nth-prime

--- exercises/practice/nth-prime/src/lib.rs
+++ exercises/practice/nth-prime/src/lib.rs
@@ -1,3 +1,23 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut num = 2;
+    let mut i = 0;
+    loop {
+        if is_prime(num) {
+            if n == i {
+                return num;
+            }
+            i += 1;
+        } else {
+        }
+        num += 1;
+    }
+}
+
+fn is_prime(n: u32) -> bool {
+    for i in 2..n {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
 }
