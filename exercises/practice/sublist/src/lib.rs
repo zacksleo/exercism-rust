33ded4e2996ff8587a8ed5fa3d6fdff209a4160b
zacksleo <zacksleo@gmail.com>
2021-06-29 16:51:05 +0800

    refactor(sublist): add sublit another solution

--- exercises/practice/sublist/src/lib.rs
+++ exercises/practice/sublist/src/lib.rs
@@ -16,6 +16,18 @@ fn contains<T: PartialEq>(a: &[T], b: &[T]) -> bool {
     contains(&a[1..], b)
 }
 
+// fn contains<T: PartialEq>(a: &[T], b: &[T]) -> bool {
+//     if a.len() < b.len() {
+//         return false;
+//     }
+//     for i in 0..a.len() {
+//         if a[i..].starts_with(b) {
+//             return true;
+//         }
+//     }
+//     false
+// }
+
 pub fn sublist<T: PartialEq>(a: &[T], b: &[T]) -> Comparison {
     if a == b {
         return Comparison::Equal;
