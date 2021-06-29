791441ced5b294cab2d78827a27d2a01201da1e1
zacksleo <zacksleo@gmail.com>
2021-06-29 23:15:59 +0800

    refact(practice): refactor nth-prime 使用函数式

--- exercises/practice/nth-prime/src/lib.rs
+++ exercises/practice/nth-prime/src/lib.rs
@@ -1,23 +1,7 @@
 pub fn nth(n: u32) -> u32 {
-    let mut num = 2;
-    let mut i = 0;
-    loop {
-        if is_prime(num) {
-            if n == i {
-                return num;
-            }
-            i += 1;
-        } else {
-        }
-        num += 1;
-    }
+    (2..).filter(|i| is_prime(*i)).nth(n as usize).unwrap_or(0)
 }
 
 fn is_prime(n: u32) -> bool {
-    for i in 2..n {
-        if n % i == 0 {
-            return false;
-        }
-    }
-    true
+    !(2..n).any(|i| n % i == 0)
 }
