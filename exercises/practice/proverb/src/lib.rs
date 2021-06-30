0baaec6124fe3716fb144b9d81df87579086e783
zacksleo <zacksleo@gmail.com>
2021-06-30 23:14:41 +0800

    feat(practice): proverb

--- exercises/practice/proverb/src/lib.rs
+++ exercises/practice/proverb/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    let len = list.len();
+
+    (0..len).fold("".to_string(), |mut cur, index| {
+        if index == len - 1 {
+            cur.push_str(&format!("And all for the want of a {}.", list[0]));
+        } else {
+            cur.push_str(&format!(
+                "For want of a {} the {} was lost.\n",
+                list[index],
+                list[index + 1]
+            ));
+        }
+        cur
+    })
 }
