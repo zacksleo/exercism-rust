eb4277b65e9370307ad285437b44791cd595fcda
zacksleo <zacksleo@gmail.com>
2021-06-30 23:15:23 +0800

    refactor(practice): 使用 windows 和 chain 方法

--- exercises/practice/proverb/src/lib.rs
+++ exercises/practice/proverb/src/lib.rs
@@ -1,16 +1,16 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    let len = list.len();
+    if list.is_empty() {
+        return String::new();
+    }
 
-    (0..len).fold("".to_string(), |mut cur, index| {
-        if index == len - 1 {
-            cur.push_str(&format!("And all for the want of a {}.", list[0]));
-        } else {
-            cur.push_str(&format!(
-                "For want of a {} the {} was lost.\n",
-                list[index],
-                list[index + 1]
-            ));
-        }
-        cur
-    })
+    let res = list
+        .windows(2)
+        .map(|x| format!("For want of a {} the {} was lost.", x[0], x[1]))
+        .chain(std::iter::once(format!(
+            "And all for the want of a {}.",
+            list[0]
+        )))
+        .collect::<Vec<_>>();
+
+    res.join("\n")
 }
