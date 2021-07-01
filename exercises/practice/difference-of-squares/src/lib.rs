6cbf9a8bcef13f9354b0375dd9f0fb3ce8b56139
zacksleo <zacksleo@gmail.com>
2021-07-01 16:48:07 +0800

    feat(practice): difference of squares

--- exercises/practice/difference-of-squares/src/lib.rs
+++ exercises/practice/difference-of-squares/src/lib.rs
@@ -1,14 +1,11 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{}", n)
+    (1 + n) * n  * (1 + n) * n / 4
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{}", n)
+    (0..n + 1).fold(0, |sum, i| sum + i*i)
 }
 
 pub fn difference(n: u32) -> u32 {
-    unimplemented!(
-        "difference between square of sum of 1...{n} and sum of squares of 1...{n}",
-        n = n,
-    )
+    square_of_sum(n) - sum_of_squares(n)
 }
