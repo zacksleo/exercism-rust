2b8e90986f77a97a8deccdf6d8d5c7b23756e3b6
zacksleo <zacksleo@gmail.com>
2021-07-01 23:15:01 +0800

    refact(practice): grains

--- exercises/practice/grains/src/lib.rs
+++ exercises/practice/grains/src/lib.rs
@@ -1,10 +1,10 @@
 pub fn square(s: u32) -> u64 {
-    if s < 1 || s > 64 {
-        panic!("Square must be between 1 and 64");
+    match s {
+        1..=64 => 2u64.pow(s - 1),
+        _ => panic!("Square must be between 1 and 64"),
     }
-    2u64.pow(s - 1)
 }
 
 pub fn total() -> u64 {
-    (2u128.pow(65) - 1) as u64
+    (1..65).map(square).sum()
 }
