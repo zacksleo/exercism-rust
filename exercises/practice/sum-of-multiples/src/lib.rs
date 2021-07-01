fbd91ae5fe78e40949e894d87fbad6ea87a8f36a
zacksleo <zacksleo@gmail.com>
2021-07-01 22:52:58 +0800

    feat(practice): sum of multiples

--- exercises/practice/sum-of-multiples/src/lib.rs
+++ exercises/practice/sum-of-multiples/src/lib.rs
@@ -1,7 +1,20 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    (1..limit)
+        .filter(|x| is_factor(*x, factors))
+        .into_iter()
+        .fold(0, |sum, x| sum + x)
+}
+
+fn is_factor(n: u32, factors: &[u32]) -> bool {
+    factors.iter().any(|x| *x != 0 && n % x == 0)
+}
+
+#[test]
+fn test_zero() {
+    assert_eq!(false, is_factor(1, &[0]));
+}
+
+#[test]
+fn test_empty(){
+    assert_eq!(false, is_factor(10000, &[]));
 }
