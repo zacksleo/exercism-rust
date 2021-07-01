5bf72da674b7ef113bf75c0839fa0445ff00646f
zacksleo <zacksleo@gmail.com>
2021-07-01 22:56:02 +0800

    refact(practice): sum of multiples

--- exercises/practice/sum-of-multiples/src/lib.rs
+++ exercises/practice/sum-of-multiples/src/lib.rs
@@ -1,20 +1,5 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     (1..limit)
-        .filter(|x| is_factor(*x, factors))
-        .into_iter()
-        .fold(0, |sum, x| sum + x)
-}
-
-fn is_factor(n: u32, factors: &[u32]) -> bool {
-    factors.iter().any(|x| *x != 0 && n % x == 0)
-}
-
-#[test]
-fn test_zero() {
-    assert_eq!(false, is_factor(1, &[0]));
-}
-
-#[test]
-fn test_empty(){
-    assert_eq!(false, is_factor(10000, &[]));
+        .filter(|x| factors.iter().any(|f| *f != 0 && x % f == 0))
+        .sum()
 }
