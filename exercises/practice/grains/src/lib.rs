721e60362f0d34d6ca4d38e7fc2f8b656efb80f7
zacksleo <zacksleo@gmail.com>
2021-07-01 23:11:42 +0800

    feat(practice): grains

--- exercises/practice/grains/src/lib.rs
+++ exercises/practice/grains/src/lib.rs
@@ -1,7 +1,10 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    if s < 1 || s > 64 {
+        panic!("Square must be between 1 and 64");
+    }
+    2u64.pow(s - 1)
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (2u128.pow(65) - 1) as u64
 }
