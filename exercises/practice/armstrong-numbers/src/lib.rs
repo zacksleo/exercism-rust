11da1d259a6456029ba1f9d1ef73b54db266be65
zacksleo <zacksleo@gmail.com>
2021-07-04 22:49:10 +0800

    feat(practice): armstrong number

--- exercises/practice/armstrong-numbers/src/lib.rs
+++ exercises/practice/armstrong-numbers/src/lib.rs
@@ -1,3 +1,6 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let str = num.to_string();
+    let len = str.len() as u32;
+    let sum: u32 = str.chars().map(|e| e.to_digit(10).unwrap().pow(len)).sum();
+    sum == num
 }
