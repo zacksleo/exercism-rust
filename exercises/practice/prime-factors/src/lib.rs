a274fe123c9c07eae8433875c40508139860042d
zacksleo <zacksleo@gmail.com>
2021-07-04 21:52:01 +0800

    feat(practice): prime factors

--- exercises/practice/prime-factors/src/lib.rs
+++ exercises/practice/prime-factors/src/lib.rs
@@ -1,3 +1,24 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    let mut res = vec![];
+    let num = n;
+    if n <= 1 {
+        return res;
+    }
+    let mut has_prime = false;
+    for cur in 2..num {
+        if num % cur == 0 {
+            has_prime = true;
+            res.push(cur);
+            if num / cur > 1 {
+                let mut res2 = factors(num / cur).clone();
+                res.append(&mut res2);
+                return res;
+            }
+        }
+    }
+    if !has_prime {
+        res.push(n);
+    }
+    res
 }
+
